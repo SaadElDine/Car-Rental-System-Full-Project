fcdb86832f8727a7a70b48d8a348a78a989abe2f
Saad El Dine Ahmed <saadeldine460@gmail.com>
2023-12-29 03:58:20 +0200

    Done AdminLogin

--- src/pages/AdminReport.jsx
+++ src/pages/AdminReport.jsx
@@ -1,28 +1,160 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Form, FormGroup, Label, Input, Button, Table } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
+import axios from "axios";
 
 import "../styles/contact.css";
 
 export function AdminReport() {
+  const navigate = useNavigate();
+  const [reportType, setReportType] = useState("allReservations");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [specificDate, setSpecificDate] = useState("");
+  const [customerId, setCustomerId] = useState("");
+  const [reportData, setReportData] = useState([]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+      let response;
+      if (reportType === "allReservations" || reportType === "reservationsByCar") {
+        response = await axios.get("http://localhost:8080/reservations", { params: data });
+      } else if (reportType === "carStatus") {
+        response = await axios.get(`http://localhost:8080/cars/status/${specificDate}`);
+      } else if (reportType === "customerReservations") {
+        response = await axios.get(`http://localhost:8080/customers/${customerId}/reservations`);
+      } else if (reportType === "dailyPayments") {
+        response = await axios.get("http://localhost:8080/payments", { params: data });
+      }
+
+      setReportData(response.data);
+      navigate("/admin/report/table");
+    } catch (error) {
+      console.error("Error fetching report data", error);
+    }
+  };
+
   return (
     <Helmet title="Admin Report">
-      {<CommonSection title="Report" />}
+      <CommonSection title="Report" />
       <section>
         <Container>
-
-              <Form>
-                <button className=" contact__btn" type="submit">
-                  Export PDF
-                </button>
-              </Form>
-
+          <Form onSubmit={onSubmit}>
+            <FormGroup>
+              <Label for="reportType">Select Report Type:</Label>
+              <Input
+                type="select"
+                name="reportType"
+                id="reportType"
+                value={reportType}
+                onChange={(e) => setReportType(e.target.value)}
+              >
+                <option value="allReservations">All Reservations</option>
+                <option value="reservationsByCar">Reservations by Car</option>
+                <option value="carStatus">Car Status</option>
+                <option value="customerReservations">Customer Reservations</option>
+                <option value="dailyPayments">Daily Payments</option>
+              </Input>
+            </FormGroup>
+            {(reportType === "allReservations" ||
+              reportType === "reservationsByCar") && (
+              <>
+                <FormGroup>
+                  <Label for="startDate">Start Date:</Label>
+                  <Input
+                    type="date"
+                    name="startDate"
+                    id="startDate"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="endDate">End Date:</Label>
+                  <Input
+                    type="date"
+                    name="endDate"
+                    id="endDate"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                  />
+                </FormGroup>
+              </>
+            )}
+            {reportType === "carStatus" && (
+              <FormGroup>
+                <Label for="specificDate">Specific Date:</Label>
+                <Input
+                  type="date"
+                  name="specificDate"
+                  id="specificDate"
+                  value={specificDate}
+                  onChange={(e) => setSpecificDate(e.target.value)}
+                />
+              </FormGroup>
+            )}
+            {reportType === "customerReservations" && (
+              <FormGroup>
+                <Label for="customerId">Customer ID:</Label>
+                <Input
+                  type="text"
+                  name="customerId"
+                  id="customerId"
+                  value={customerId}
+                  onChange={(e) => setCustomerId(e.target.value)}
+                />
+              </FormGroup>
+            )}
+            {reportType === "dailyPayments" && (
+              <>
+                <FormGroup>
+                  <Label for="startDate">Start Date:</Label>
+                  <Input
+                    type="date"
+                    name="startDate"
+                    id="startDate"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="endDate">End Date:</Label>
+                  <Input
+                    type="date"
+                    name="endDate"
+                    id="endDate"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                  />
+                </FormGroup>
+              </>
+            )}
+            <button className="contact__btn" type="submit">
+              Get Report
+            </button>
+          </Form>
+          {reportData.length > 0 && (
+            <Table>
+              <thead>
+                {/* Add table header cells based on report type */}
+              </thead>
+              <tbody>
+                {reportData.map((row, index) => (
+                  <tr key={index}>
+                    {/* Render table cells based on report type */}
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
         </Container>
       </section>
     </Helmet>
   );
-};
-
-
+}
